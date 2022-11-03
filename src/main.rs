6792f0b723cb90c2c82b0c923f8ef0055122a250
Thomas Haller <Thomas.Haller@lab10.coop>
2022-11-03 14:10:08 +0100

    testcode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{error::Error, net::SocketAddr, thread::sleep, time::Duration};
+
+use mio::net::{TcpListener};
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    
+    // Setup the server socket.
+    let addr =  SocketAddr::from(([127, 0, 0, 1], 30301));
+
+    match TcpListener::bind(&addr) {
+        Ok(tcp_listener) => {
+            //let x = mio::sys::TcpListener::new(listener);
+            println!("Listening on: {}", addr);
+            //let x  = SocketAddr::new(addr.ip(), addr.port());
+            sleep(Duration::from_secs(100));
+        }
+        Err(e) => {
+            println!("Failed to bind server socket: {}", e);
+        }
+    }
+
+    
+    
 }
